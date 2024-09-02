c2dac045c26233c7c6e3b58c15bd86ec24f466ec
NgunyiGachie <antogachie@gmail.com>
2024-09-02 14:28:06 +0300

    add css

--- frontend/my-portfolio/src/components/NavBar.js
+++ frontend/my-portfolio/src/components/NavBar.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react';
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
@@ -26,7 +30,7 @@ export const NavBar = () => {
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
         <Navbar.Brand href="#home">
-          <img src="/path-to-your-logo.png" alt="logo" />
+          <img src={logo} alt="logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className='navbar-toggler-icon'></span>
@@ -39,15 +43,9 @@ export const NavBar = () => {
           </Nav>
           <span className='navbar-text'>
             <div className='social-icon'>
-              <a href="https://example.com" aria-label="Social Link">
-                <img src="/path-to-social-icon.png" alt="Social Icon" />
-              </a>
-              <a href="https://example.com" aria-label="Social Link">
-                <img src="/path-to-social-icon.png" alt="Social Icon" />
-              </a>
-              <a href="https://example.com" aria-label="Social Link">
-                <img src="/path-to-social-icon.png" alt="Social Icon" />
-              </a>  
+                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
             </div>
             <button className='vvd' onClick={() => console.log('connect')}><span>Let's Connect</span></button>
           </span>
