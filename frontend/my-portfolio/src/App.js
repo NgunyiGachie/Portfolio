77306c4683f82c90ffe987e56ae4404c82659f4f
NgunyiGachie <antogachie@gmail.com>
2024-09-03 13:32:26 +0300

    Update components

--- frontend/my-portfolio/src/App.js
+++ frontend/my-portfolio/src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
@@ -6,6 +5,8 @@ import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
+
 function App() {
   return (
     <div className="App">
@@ -14,6 +15,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
